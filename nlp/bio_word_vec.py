660f21e221f24afeaa5190fbe3573672f82459e0
Juhong Park <juhongp@mit.edu>
2026-03-27 18:50:25 -0400

    feat: add notebooks, data docs, and update nlp module

--- nlp/bio_word_vec.py
+++ nlp/bio_word_vec.py
@@ -1,5 +1,19 @@
+import os
 from gensim.models import KeyedVectors
-filename = '/home/mimic/Downloads/BioWordVec/BioWordVec_PubMed_MIMICIII_d200.vec.bin' 
-model = KeyedVectors.load_word2vec_format(filename, binary=True)
-vector = model.get_vector('influenza')
-print(vector)
+
+BIOWORDVEC_PATH = os.environ.get(
+    "BIOWORDVEC_PATH",
+    "/home/mimic/Downloads/BioWordVec/BioWordVec_PubMed_MIMICIII_d200.vec.bin",
+)
+
+
+def load_biowordvec(path=None):
+    if path is None:
+        path = BIOWORDVEC_PATH
+    return KeyedVectors.load_word2vec_format(path, binary=True)
+
+
+if __name__ == '__main__':
+    model = load_biowordvec()
+    vector = model.get_vector('influenza')
+    print(vector)
